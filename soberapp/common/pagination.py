a152f1286e309a5929d585c9df997fd0107558f7
Om Mishra <ommishra32868@gmail.com>
2022-11-20 20:49:43 +0530

    Reverting back to old format

--- soberapp/common/pagination.py
+++ soberapp/common/pagination.py
@@ -4,4 +4,7 @@ from rest_framework.response import Response
 
 class AppPagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
-        return Response(data)
+        return Response({
+            'total_records': self.page.paginator.count,
+            'data': data
+        })
