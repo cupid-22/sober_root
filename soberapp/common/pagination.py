bf4f8d82d52074c56cec8cb4b19e6bd7ade183f8
Om Mishra <ommishra32868@gmail.com>
2022-11-20 20:42:52 +0530

    Response format alteration

--- soberapp/common/pagination.py
+++ soberapp/common/pagination.py
@@ -4,7 +4,4 @@ from rest_framework.response import Response
 
 class AppPagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
-        return Response({
-            'total_records': self.page.paginator.count,
-            'data': data
-        })
+        return Response(data)
